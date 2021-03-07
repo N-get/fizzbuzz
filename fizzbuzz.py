235ae22c4fa9a2d89a40b78162de584729aa3c39
N-get <56210244+N-get@users.noreply.github.com>
2021-03-07 15:53:08 -0800

    Add files via upload
    Realized I misread the instructions, added for loop in fizzbuzz.py that does the printing instead of the function.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,18 @@
 
 def fizzbuzz(i):
     if(i % 3 == 0 and i % 5 == 0):
-        print("FizzBuzz")
+        #print("FizzBuzz")
         return "FizzBuzz"
     elif(i % 3 == 0):
-        print("Fizz")
+        #print("Fizz")
         return "Fizz"
     elif(i % 5 == 0):
-        print("Buzz")
+        #print("Buzz")
         return "Buzz"
     else:
-        print(i)
+        #print(i)
         return i
        
+
+for i in range(1, 101):
+    print(fizzbuzz(i))
