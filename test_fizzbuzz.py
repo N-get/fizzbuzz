f57c29d93bc2583c0b8fb3926ba6bca6b7358ed2
N-get <56210244+N-get@users.noreply.github.com>
2021-03-07 15:33:51 -0800

    Add files via upload
    Added test for numbers divisible by 3 to check that "fizz" is returned

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,8 +4,13 @@ import fizzbuzz
 class fizz(unittest.TestCase):
     def test_fizzbuzz(self):
         for i in range(100):
+            if (i == 0):
+                i = 1
             result = fizzbuzz.fizzbuzz(i)
-            self.assertEqual(result, i)
+            if(i % 3 == 0):
+                self.assertEqual(result, "fizz")
+            else:
+                self.assertEqual(result, i)
 
 
 if __name__ == "__main__":
